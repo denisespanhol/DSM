2917c3cb62eeed2e82fab11dda725e49b136d3f5
Denis Guilherme Ferreira Espanhol <denisf@ciandt.com>
2026-07-21 14:02:34 -0300

    feat: change DSMColors  background color to UICOLOR.systemBackground

--- Sources/DSM/Tokens/DSMColors.swift
+++ Sources/DSM/Tokens/DSMColors.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 public enum DSMColors {
     public static let primary = Color("PrimaryColor", bundle: .module)
-    public static let background = Color("BackgroundColor", bundle: .module)
+    public static let background = Color(UIColor.systemBackground)
     public static let error = Color.red
     public static let textPrimary = Color.primary
     public static let textSecondary = Color.secondary
